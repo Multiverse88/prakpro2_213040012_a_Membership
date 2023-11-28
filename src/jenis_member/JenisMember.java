27afa246aecc2d58599dac3c6e11217690c0389b
Multiverse88 <89334493+Multiverse88@users.noreply.github.com>
2023-11-28 13:13:12 +0700

    tugas praktikum pemprograman 2 pertemuan 9

---
+++ src/jenis_member/JenisMember.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jenis_member;
+
+/**
+ *
+ * @author Ainan
+ */
+
+public class JenisMember {
+    private String id;
+    private String nama;
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getId() {
+        return this.id;
+    } 
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return this.nama;
+    } 
+}
---
+++ src/member/Member.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package member;
+
+import jenis_member.JenisMember;
+
+/**
+ *
+ * @author Ainan
+ */
+
+public class Member {
+    private String id;
+    private String nama;
+    private JenisMember jenisMember;
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getId() {
+        return this.id;
+    } 
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+
+    public String getNama() {
+        return this.nama;
+    }   
+    
+    public void setJenisMember(JenisMember jenisMember) {
+
+        this.jenisMember = jenisMember;
+    }
+    
+    public JenisMember getJenisMember() {
+        return this.jenisMember;
+    }   
+}
